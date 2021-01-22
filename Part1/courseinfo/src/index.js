7ea0f56a935ab3e4f6bf256a359461fa36a509b9
My Name <oscar.vonnandelstadh@centria.fi>
2021-01-22 16:15:56 +0200

    1.5 courseinformation single JS object

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -2,49 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
+  const object = {
+    course: 'Half Stack application development',
+    parts: {
+      first: {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      second: {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      third: {
+        name: 'State of a component',
+        exercises: 14
+      }
     },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
-  const Header = (props) => {
-    return (
-      <h1>{props.course}</h1>
-    )
-  }
-  const Content = (props) => {
-    console.log("Contents props: ", props)
-    return (
-      <div>
-        <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-        <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-        <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-      </div>
-    )
-  }
-  const Total = (props) => {
-    console.log("Total props: ", props)
-    return (
-      <p>{props.props[0].exercises + props.props[1].exercises + props.props[2].exercises}</p>
-    )
+    exercises: function() { return this.parts.first.exercises + this.parts.second.exercises + this.parts.third.exercises;}
   }
   
   return (
     <div>
-      <Header header={course}/>
-      <Content parts={parts}/>
-      <Total props={parts}/>
+      <h1>{object.course}</h1>
+      <p>{object.parts.first.name} {object.parts.first.exercises}</p>
+      <p>{object.parts.second.name} {object.parts.second.exercises}</p>
+      <p>{object.parts.third.name} {object.parts.third.exercises}</p>
+      <p>Total Exercises: {object.exercises()}</p>
     </div>
   )
 }
