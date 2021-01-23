ad57c7a31ecd886337420bc86c270d4aa1d53363
My Name <oscar.vonnandelstadh@centria.fi>
2021-01-23 13:10:53 +0200

    1.12 anecdotes step1

--- Part1/anecdotes/src/index.js
+++ Part1/anecdotes/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const random = () => {
+      // return Math.floor(Math.random * anecdotes.length)
+      // comment out multiple lines with Shift+Alt+A
+/*       console.log("Random")
+      console.log(Math.random() * anecdotes.length)
+      console.log(Math.floor(Math.random() * anecdotes.length))
+      console.log("-----") */
+      // so Math.random requires () to be called, probably because it is a named method and not written in React
+      return Math.floor(Math.random() * anecdotes.length)
+  }
+
+  const Button = ({method, text}) => {
+    return (
+      <button onClick={method}>{text}</button>
+    )
+  }
+
+  return (
+    <div>
+      {console.log(selected)}
+      <p>{props.anecdotes[selected]}</p>
+      <p><Button method={() => setSelected(random)} text="random"/></p>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
