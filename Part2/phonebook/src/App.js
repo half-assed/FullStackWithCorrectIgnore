cdbaaa7ead8d0deae102bbb16a482bb8b5d03cf2
My Name <oscar.vonnandelstadh@centria.fi>
2021-01-26 17:10:15 +0200

    2.8 the phonebook step3

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,30 +2,36 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', key: 'Arto Hellas'}
-  ]) 
+    { name: 'Arto Hellas', number: '01234567890', key: '01234567890'}
+  ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const clickHandler = (event) => {
     event.preventDefault()
-    const name = event.target.value
 
-    if (name === "") {
-      console.log("The name field is empty.")
+    const index = persons.map(element => element.key).indexOf(newNumber)
+    if (newName === "" || newNumber==='') {
+      console.log("Fields cannot be left empty.")
     }
-    else if (persons.map(element => element.key).indexOf(name) !== -1) {
-      alert(`${name} is already added to phonebook`)
+    else if ( index !== -1) {
+      alert(`${newNumber} is the number of ${persons[index].name}`)
     }
     else {
-      console.log(name)
-      setPersons(persons.concat({name: name, key: name}))
+      setPersons(persons.concat({name: newName, number: newNumber, key: newNumber}))
       setNewName('')
+      setNewNumber('')
     }
   }
 
-  const onChangeHandler = (event) => {
+  const onChangeHandlerName = (event) => {
     return (
       setNewName(event.target.value)
+    )
+  }
+  const onChangeHandlerNumber = (event) => {
+    return (
+      setNewNumber(event.target.value)
     )
   }
 
@@ -33,17 +39,13 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={onChangeHandler}/>
-        </div>
-        <div>
-
-          <button type="submit" value={newName} onClick={clickHandler}>add</button>
-        </div>
+        <div>name: <input value={newName} onChange={onChangeHandlerName}/></div>
+        <div>number: <input value={newNumber} onChange={onChangeHandlerNumber}/></div>
+        <div><button type="submit" value={""} onClick={clickHandler}>add</button></div>
       </form>
       
       <h2>Numbers</h2>
-      <div>{persons.map((element, key) => <p key={key}>{element.name}</p>)}</div>
+      <div>{persons.map((element, key) => <p key={element.number}>{element.name} - {element.number}</p>)}</div>
     </div>
   )
 }
