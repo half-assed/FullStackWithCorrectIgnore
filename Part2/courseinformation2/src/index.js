96249921cfe8f855d75fae989357aa03c02d25e6
My Name <oscar.vonnandelstadh@centria.fi>
2021-01-26 08:44:55 +0200

    2.5 separate module

--- Part2/courseinformation2/src/index.js
+++ Part2/courseinformation2/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
@@ -52,45 +53,5 @@ const App = () => {
       </div>
     )
   }
-
-const Course = (props) => {
-  console.log("Course props: ", props)
-  const course = props.props
-  return (
-    <div>
-        <Header props={course.name}/>
-        <Content props={course.parts} />
-        <Total props={course.parts}/>
-    </div>
-  )
-}
-
-const Header = (props) => {
-  console.log("Header props: ", props)
-  return (
-    <h2>{props.props}</h2>
-  )
-}
-
-const Content = (props) => {
-    console.log("Content props: ", props)
-    const array = props.props
-    return <div>{array.map((element, key) => { return <Part props={element} key={element.id}/> })}</div>
-}
-const Part = (props) => {
-  console.log("Part props: ", props)
-  const imTiredofProps = props.props
-  return (
-    <p>{imTiredofProps.name} {imTiredofProps.exercises}</p>
-  )
-}
-
-const Total = (props) => {
-  console.log("Total props: ", props)
-  return (
-    <p>Total of <strong>{props.props.reduce((sum, element) => sum += element.exercises, 0)}</strong> exercises</p>
-  )
-}
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
