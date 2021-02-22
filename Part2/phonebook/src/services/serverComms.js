819cdb87f3f5b016f0ac4d81e0d5b1d66fce8cd8
My Name <oscar.vonnandelstadh@centria.fi>
2021-02-22 15:20:14 +0200

    updated URL

--- Part2/phonebook/src/services/serverComms.js
+++ Part2/phonebook/src/services/serverComms.js
@@ -2,19 +2,19 @@ import axios from 'axios'
 
 const getAll = (props) => {
     console.log(props)
-    return axios.get(`http://localhost:3001/persons`) // persons is an element(/container) in the db.json file, 3001 is the port defined for json-server when installed with: npx json-server --port 3001 --watch db.json
+    return axios.get(`http://localhost:3001/api/persons`) // persons is an element(/container) in the db.json file, 3001 is the port defined for json-server when installed with: npx json-server --port 3001 --watch db.json
         .then(response => response.data )
 }
 
 const create = (props) => {
     console.log("Server Create props: ", props)
-    return axios.post(`http://localhost:3001/persons`, {name: props[0], number: props[1]})
+    return axios.post(`http://localhost:3001/api/persons`, {name: props[0], number: props[1]})
         .then(response => response.data )
 }
 
 const update = (props) => {
     console.log("Server Update props: ", props)
-    return axios.put(`http://localhost:3001/persons/${props[0]}`, {name: props[1], number: props[2]})
+    return axios.put(`http://localhost:3001/api/persons/${props[0]}`, {name: props[1], number: props[2]})
         .then(response => response.data)
 }
 
